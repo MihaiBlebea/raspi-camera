31aeb34e57258af50d53a448795f41b151fb7955
Mihai Blebea <mihai@getchip.uk>
2020-08-28 22:38:35 +0100

    Changes timelapse interval

--- timelapse.py
+++ timelapse.py
@@ -4,6 +4,6 @@ from picamera import PiCamera
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
-for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+for filename in camera.capture_continuous('/img/img{counter:03d}.jpg'):
     print('Captured %s' % filename)
-    sleep(300) # wait 5 minutes
+    sleep(30) # wait 5 minutes
