9dce519f7cb7c0c4005c4482f00ed165d797bc63
Mihai Blebea <mihaiserban.blebea@gmail.com>
2020-08-28 21:44:52 +0100

    First commit

---
+++ raspicamera.py
@@ -0,0 +1,13 @@
+from picamera import PiCamera
+from time import sleep
+
+# Explicitly open a new file called my_image.jpg
+my_file = open('my_image.jpg', 'wb')
+camera = PiCamera()
+
+camera.start_preview()
+sleep(2)
+camera.capture(my_file)
+# At this point my_file.flush() has been called, but the file has
+# not yet been closed
+my_file.close()
---
+++ upload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp ./raspicamera.py pi@mini:/
